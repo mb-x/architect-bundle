96201f20538cd0cee992a58d3ad4bbbc24c433a4
bengrich <webmasterbengrich@gmail.com>
2016-11-27 21:02:09 +0000

    MIT license

--- Command/GeneratorCommand.php
+++ Command/GeneratorCommand.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the ArchitectBundle package.
+ *
+ * (c) mb-x <https://github.com/mb-x/architect-bundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Mbx\ArchitectBundle\Command;
 
 use Mbx\ArchitectBundle\Generator\MbxGenerator;
