935ba0d4120a8a6f6524c32ab720de35bb4d6a17
bengrich <webmasterbengrich@gmail.com>
2016-12-04 15:47:30 +0000

    Add Extra Vars for FormHandler

--- Abstracts/AbstractFormHandler.php
+++ Abstracts/AbstractFormHandler.php
@@ -101,9 +101,9 @@ abstract class AbstractFormHandler implements FormHandlerInterface {
     public function processForm(EntityInterface $entity) {
         $this->createForm($entity);
         $this->form->handleRequest($this->request);
-        $this->beforeCheckForm($entity);
+        $extraVars = $this->beforeCheckForm($entity);
         if ($this->form->isSubmitted() && $this->form->isValid()) {
-            $this->afterCheckForm($entity);
+            $this->afterCheckForm($entity, $extraVars);
             $this->manager->save($entity);
             return true;
         }
@@ -120,9 +120,9 @@ abstract class AbstractFormHandler implements FormHandlerInterface {
     public function processDeleteForm(EntityInterface $entity) {
         $form = $this->createDeleteForm($entity);
         $form->handleRequest($this->request);
-        $this->beforeCheckDeleteForm($entity);
+        $extraVars = $this->beforeCheckDeleteForm($entity);
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->afterCheckDeleteForm($entity);
+            $this->afterCheckDeleteForm($entity, $extraVars);
             $this->manager->remove($entity);
             return true;
         }
